e3590f88b1826d0798bf116022ff1861c3b0b0ee
iamkyu <kyujnam@gmail.com>
2017-05-29 12:28:56 +0900

    refactor: UserDao를 통해 JdbcContext에 dataSource를 주입

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -20,11 +20,9 @@ public class UserDao {
 
     private JdbcContext jdbcContext;
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
-
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+        this.jdbcContext.setDataSource(dataSource);
         this.dataSource = dataSource;
     }
 
