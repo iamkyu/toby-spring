d7a99e852d2d4162d4d4fe3a0e66124071829976
iamkyu <kyujnam@gmail.com>
2017-06-01 08:55:13 +0900

    UserService 추가/수정 구현

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -1,11 +1,19 @@
 package springbook.user.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -15,10 +23,57 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class UserServiceTest {
+
     @Autowired UserService userService;
+    @Autowired UserDao userDao;
+    private List<User> users;
+
+    @Before
+    public void setUp() {
+        userDao.deleteAll();
+
+        users = Arrays.asList(
+                new User("amy", "에이미", "p1", Level.BASIC, 49, 0),
+                new User("joshlong", "조시롱", "p2", Level.BASIC, 50, 0),
+                new User("mark", "마크", "p3", Level.SILVER, 60, 29),
+                new User("miller", "밀러", "p4", Level.SILVER, 60, 30),
+                new User("lloyd", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+    }
 
     @Test
-    public void bean() {
-        assertNotNull(userService);
+    public void upgradeLevels() {
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
+
+    @Test
+    public void add() {
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+        assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
     }
 }
