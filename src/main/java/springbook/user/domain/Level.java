106ff7167565ffa8b2fa503570b6f20c7fd56e9e
iamkyu <kyujnam@gmail.com>
2017-06-02 08:31:41 +0900

    refactor: 회원 레벨 업그레이드 판단 로직을 분리

--- src/main/java/springbook/user/domain/Level.java
+++ src/main/java/springbook/user/domain/Level.java
@@ -5,16 +5,22 @@ package springbook.user.domain;
  * @since 2017-06-01
  */
 public enum  Level {
-    BASIC(1), SILVER(2), GOLD(3);
+    GOLD(3, null), SILVER(2, GOLD), BASIC(1, SILVER);
 
     private final int value;
+    private final Level next;
 
-    Level(int value) {
+    Level(int value, Level next) {
         this.value = value;
+        this.next = next;
     }
 
     public int intValue() {
         return value;
+    }
+
+    public Level nextLevel() {
+        return next;
     }
 
     public static Level valueOf(int value) {
