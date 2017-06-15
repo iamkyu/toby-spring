33665abefe5ddf5611c32bd8b5fcb7f16e5d1188
iamkyu <kyujnam@gmail.com>
2017-06-16 08:08:51 +0900

    위임을 통한 BaseSqlService 재사용

--- src/main/java/springbook/user/service/OxmSqlService.java
+++ src/main/java/springbook/user/service/OxmSqlService.java
@@ -21,7 +21,6 @@ import java.io.IOException;
  */
 public class OxmSqlService implements SqlService {
     private final BaseSqlService baseSqlService = new BaseSqlService();
-
     private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
 
     private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
